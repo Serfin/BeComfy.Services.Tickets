913fd5e19b48c1af56d4ac518d2b88e9ee26d1f5
serfin <gracjanbrylka@gmail.com>
2020-01-05 16:55:47 +0100

    Added migrations to repo

---
+++ src/BeComfy.Services.Tickets/Migrations/20191208154613_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeComfy.Services.Tickets.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FlightId = table.Column<Guid>(nullable: false),
+                    Owner = table.Column<Guid>(nullable: false),
+                    Seats = table.Column<string>(nullable: true),
+                    TotalCost = table.Column<decimal>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
---
+++ src/BeComfy.Services.Tickets/Migrations/20191208154753_ChangedColumnType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeComfy.Services.Tickets.Migrations
+{
+    public partial class ChangedColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalCost",
+                table: "Tickets",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalCost",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+    }
+}
