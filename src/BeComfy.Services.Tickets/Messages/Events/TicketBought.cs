63a54e75572e9e2cc20daf31d13ad4a2ce6f40c9
serfin <gracjanbrylka@gmail.com>
2020-01-29 21:09:08 +0100

    Fixed empty customer id

--- src/BeComfy.Services.Tickets/Messages/Events/TicketBought.cs
+++ src/BeComfy.Services.Tickets/Messages/Events/TicketBought.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using BeComfy.Common.CqrsFlow;
-using BeComfy.Common.Types.Enums;
 using Newtonsoft.Json;
 
 namespace BeComfy.Services.Tickets.Messages.Events
@@ -16,7 +14,7 @@ namespace BeComfy.Services.Tickets.Messages.Events
         public TicketBought(Guid id, Guid customerId, decimal totalPrice)
         {
             Id = id;
-            CustomerId = CustomerId;
+            CustomerId = customerId;
             TotalPrice = totalPrice;
         }
     }
