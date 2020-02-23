9d368c80e84c723a24767a53927731a7bfdba6c0
serfin <serfin009@gmail.com>
2020-02-23 03:33:02 +0100

    Renamed services

--- src/BeComfy.Services.Tickets/Proxies/ServiceModels/Flight.cs
+++ src/BeComfy.Services.Tickets/Proxies/ServiceModels/Flight.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using BeComfy.Common.Types.Enums;
 
 namespace BeComfy.Services.Tickets.Services.ServiceModels
@@ -6,6 +7,7 @@ namespace BeComfy.Services.Tickets.Services.ServiceModels
     public class Flight
     {
         public Guid Id { get; set; }
+        public IDictionary<SeatClass, int> AvailableSeats { get; set; }
         public FlightStatus FlightStatus { get; set; }
     }
 }
